df1caabab5947159ba9e9758c47464bbbfc4ca99
Jacek Mosakowski <Jacek Mosakowski@DESKTOP-RIS73BT>
2019-03-24 10:21:50 +0100

    Comments expansion

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace MP3Listing
     {
         static void Main(string[] args)
         {
-            // List all files in all subdirectories
+            // List all files in a given directory and all its subdirectories
             string currentDir = Directory.GetCurrentDirectory();
             int currentDirLen = currentDir.Length;
             string[] allFiles = Directory.GetFiles(currentDir, "*.*", SearchOption.AllDirectories);
             List<string> finalFiles = new List<string>();
 
-            // Remove unwanted entries
+            // Remove files with unwanted names
             string[] unwantedNames = { "MP3Listing.exe", "mp3.txt" };
             foreach (string file in allFiles)
             {
-                // +1 to remove initial "/"
+                // +1 in file.Remove to remove initial "/"
                 string fileShort = file.Remove(0, currentDirLen + 1);
                 bool containsForbidden = false;
                 foreach (string unwantedName in unwantedNames)
@@ -32,7 +32,7 @@ namespace MP3Listing
                     finalFiles.Add(fileShort);
             }
 
-            // Save the list to a txt file
+            // Save the MP3 list to a txt file
             TextWriter tw = new StreamWriter("mp3.txt");
             foreach (string finalFile in finalFiles)
                 tw.WriteLine(finalFile);
